707923b98c9ab1d427455da9de6b543aa3a9d8ca
ksw-92 <ksw92@scarletmail.rutgers.edu>
2025-02-26 21:30:25 -0500

    created orb drag script

---
+++ Assets/__Scripts/OrbDrag.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class OrbDrag : MonoBehaviour
+{
+    private bool isDragging = false;
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+        isDragging = true;
+        offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void OnMouseDrag()
+    {
+        if (isDragging)
+        {
+            Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+            transform.position = new Vector3(newPosition.x, newPosition.y, 0);
+        }
+    }
+
+    void OnMouseUp()
+    {
+        isDragging = false;
+    }
+}
